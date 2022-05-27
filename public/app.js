86a628095deea01e3927ced9cc4640e96ec67275
Elu Gonzalez <elugon10@gmail.com>
2022-05-27 20:05:13 +0200

    fixs on routs

--- public/app.js
+++ public/app.js
@@ -5,15 +5,15 @@ const app = express();
 app.use(express.static('public'));
 
 app.get("/Home", (req, res, next) => {
-    res.sendFile(__dirname + "/public/views/Home.html");
+    res.sendFile(__dirname + "/views/Home.html");
   });
   
   app.get("/About", (req, res, next) => {
-    res.sendFile(__dirname + "/public/views/About.html");
+    res.sendFile(__dirname + "/views/About.html");
   });
   
   app.get("/Works", (req, res, next) => {
-    res.sendFile(__dirname + "/public/views/Works.html");
+    res.sendFile(__dirname + "/views/Works.html");
   });
   
   app.listen(3000);
